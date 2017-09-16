41bcb51905ac63a8535fef3598e6ab17461e0859
Tom van der Laan <t.vanderlaan@live.nl>
2017-09-16 17:51:56 +0200

    hello worls

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
+
+
 # use the decorator pattern to
 # link the view function to a url
 @app.route("/")
@@ -13,7 +18,23 @@ app = Flask(__name__)
 
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, Worldjijijjijij!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+    
+@app.route('/name/<name>')
+
+def index(name):
+	if name.lower() == "michael" :
+		return "Hello, {}".format(name), 200
+		
+	else:
+		return "Not found", 400
+
+
 
 # start the development server using the run() method
 if __name__ == "__main__":
